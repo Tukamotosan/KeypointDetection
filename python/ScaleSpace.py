a22c52d1ecb28f42a9d3150290128f9b945a0c05
WizTukamotosan <hamaantama@gmail.com>
2017-07-02 23:52:07 +0900

    add grtayscale convert code

--- python/ScaleSpace.py
+++ python/ScaleSpace.py
@@ -1,7 +1,20 @@
 # coding: utf-8
 
+import cv2
+import numpy as np
+
 def main():
-    pass
+    path_src = "../images/lena_std.tif"
+
+    # load image
+    img_src = cv2.imread(path_src, 1)
+
+    # convert from RGB to grayscale
+    img_gry = cv2.cvtColor(img_src, cv2.COLOR_RGB2GRAY)
+
+    cv2.imshow("Image", img_gry)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
